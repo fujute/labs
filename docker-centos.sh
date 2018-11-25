6933f026025efd50a105ddafc709583e440f57ff
fujute <fuju@narisa.com>
2018-11-26 00:08:54 +0700

    Update docker-centos.sh

--- docker-centos.sh
+++ docker-centos.sh
@@ -2,6 +2,6 @@
 # https://docs.docker.com/install/linux/docker-ce/centos/#set-up-the-repository
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo usermod -aG docker your-user
+sudo usermod -aG docker $(whoami)
 sudo systemctl start docker
 yum list docker-ce --showduplicates | sort -r	
